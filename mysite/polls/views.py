03677fa117bbed358e54a20b798079848defa874
Siddharth Aadarsh <siddharth.aadarsh@healthify.co>
2021-06-19 01:53:06 +0530

    making urls loosely typed in templates and updating urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,12 +12,12 @@ def index(request):
 
 def detail(request, question_id):
 
-    q = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html',{'question': q })
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html',{'question': question })
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id) 
+    return HttpResponse("You're voting on question %s" % question_id)
