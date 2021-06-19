a21d290f0970b5681dcbcc209bbfde1a8e4e50a0
Siddharth Aadarsh <siddharth.aadarsh@healthify.co>
2021-06-19 13:54:36 +0530

    formatting code-base according to flake8

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=400)
     pub_date = models.DateTimeField('date published')
@@ -13,6 +15,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date <= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
